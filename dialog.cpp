a04422810cffa9d73c4194e7d23604c566a9a266
shavaun1 <jshavaun@yahoo.com>
2022-12-19 21:12:50 -0500

    update

--- dialog.cpp
+++ dialog.cpp
@@ -15,6 +15,15 @@
 #include<cstdlib>
 
 
+//simple conversational dialog program. Your program should
+//do the following:
+//Say “Hello” to the user.
+//Ask them if they are having a good day, and record their input.
+//If their response is anything other than a ‘y’ for yes or an ‘n’ for no,
+//repeat the question.
+//If they respond with a ‘y’, output “I’m glad to hear that”, and if they
+//answer with an ‘n’, then output “I hope your day gets better soon.”
+
 int main()
 {
   
