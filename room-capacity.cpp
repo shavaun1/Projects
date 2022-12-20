e7d07cbebfe6d0f9430d1ee7d1509454c95791b1
shavaun1 <jshavaun@yahoo.com>
2022-12-19 21:04:56 -0500

    update

--- room-capacity.cpp
+++ room-capacity.cpp
@@ -15,6 +15,21 @@
 #include<cstdlib>
 
 
+
+
+
+//a program that determines whether a meeting room is in violation of
+//fire law regulations regarding the maximum room capacity. The program
+//will read in the maximum room capacity and the number of people at-
+//tending the meeting. If the number of people is less than or equal to the
+//maximum room capacity, the program announces that it is legal to hold
+//the meeting and tells how many additional people may legally attend. If
+//the number of people exceeds the maximum room capacity, the program
+//announces that the meeting cannot be held as planned due to fire regula-
+//tions and tells how many people must be excluded in order to meet the
+//fire regulations.
+
+
 int main()
 {
   
