c6405918bc4a1ed62ac4b18104b3a36cae68bd1d
shavaun1 <jshavaun@yahoo.com>
2023-04-18 21:07:33 -0400

    new

--- Practice-make-perfect/christmas-tree.cpp
+++ Practice-make-perfect/christmas-tree.cpp
@@ -5,12 +5,36 @@ using namespace std;
 
 int main()
 {
-  int array[] = {4,8,1,56,73,36,99,21};
-  int arrayLength = sizeof(array) / sizeof(*array);
+  int n = 10;
 
-  for(int i = 0; i < arrayLength; i++)
+  for(int i = 1; i <=n; i++)
   {
-    cout << array[i] << endl;
+    
+    for(int j = n-i; j >0; j--)
+    
+      cout << " ";
+
+    for(int k = 1; k <= i; k++)
+    
+      cout <<"@ ";
+
+     cout << "\n";
+
   }
+
+
+  for(int a = n; a >= 1; a--)
+  {
+    for(int b = n-a; b >0; b--)
+
+      cout<<" ";
+
+   for(int c = 0; c <= a; c++)
+    cout<<"+ ";
+    cout<<"\n";
+  }
+
 }
+
+
 
