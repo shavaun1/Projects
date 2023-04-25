b00a22e273ac8a0bee466680f8d05697bbca3cad
shavaun1 <jshavaun@yahoo.com>
2023-04-25 18:11:30 -0400

    new

--- Practice-make-perfect/christmas-tree.cpp
+++ Practice-make-perfect/christmas-tree.cpp
@@ -3,38 +3,19 @@
 
 using namespace std;
 
+void DoSomething();
+
 int main()
 {
-  int n = 10;
-
-  for(int i = 1; i <=n; i++)
-  {
-    
-    for(int j = n-i; j >0; j--)
-    
-      cout << " ";
-
-    for(int k = 1; k <= i; k++)
-    
-      cout <<"@ ";
-
-     cout << "\n";
-
-  }
-
-
-  for(int a = n; a >= 1; a--)
-  {
-    for(int b = n-a; b >0; b--)
-
-      cout<<" ";
-
-   for(int c = 0; c <= a; c++)
-    cout<<"+ ";
-    cout<<"\n";
-  }
-
+  DoSomething();
 }
+
+
+void DoSomething()
+{
+  cout<<"Hello, I am learning how to code properly, I feel happy because of it.";
+}
+
 
 
 
