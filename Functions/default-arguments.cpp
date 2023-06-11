8e1527f88771848bb97951e606548ad67a8e7d10
shavaun1 <jshavaun@yahoo.com>
2023-06-11 19:08:16 -0400

    update

---
+++ Functions/default-arguments.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void showVolume(int length, int width = 1, int height = 1);
+//Returns the volume of a box
+//if no height is given, the height is assumed to be 1.
+//If neither height nor width is given, both are assumed to be 1.
+
+int main()
+{
+  showVolume(4,6,2);
+  showVolume(4,6);
+  showVolume(4);
+
+  return 0;
+}
+
+void showVolume(int length, int width, int height)
+{
+  cout<<"Volume of a box with \n"
+    <<"Length = "<< length <<" ,Width = "<<width << endl
+    <<"and Heigth = "<< height
+    <<" is "<<length*width*height<< endl;
+}
