e91f9267a964dfd2be7a080de48ad3ba966dfd6e
shavaun1 <jshavaun@yahoo.com>
2024-11-21 21:03:16 -0600

    update

---
+++ experiment/heads/LibraryStore.cpp
@@ -0,0 +1,22 @@
+#include"LibraryStore.h"
+#include<list>
+#include<string>
+#include<iostream>
+using std::cout;using std::cin;
+using std::endl; using std::list;
+using std::iterator;
+//creating a two genres, with their subgenres.
+string genre() 
+{
+  list<string>fiction, non-fiction;
+  list<string>iterator it;
+
+  for()
+
+
+  
+  cout<<"Please pick a genre to add your book(s) to: ";
+
+  list<string>genre;
+
+}
---
+++ experiment/heads/LibraryStore.h
@@ -0,0 +1,18 @@
+#ifndef LIBRARYSTORE_H
+#define LIBRARYSTORE_H
+#include<string>
+using std::string;
+
+class Librarystore{
+  private:
+
+
+  public:
+    string genre();
+    void title();
+    void author();
+
+
+};
+
+#endif 
