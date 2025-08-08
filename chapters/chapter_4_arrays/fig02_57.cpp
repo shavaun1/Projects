f32c38c728c546ba09e80b61cd7d8dac2476e6cb
shavaun1 <jshavaun@yahoo.com>
2025-08-08 18:50:12 +0300

    update

---
+++ chapters/chapter_4_arrays/fig02_57.cpp
@@ -0,0 +1,35 @@
+#include <array>
+#include<format>
+#include<iostream>
+#include<numeric>
+#include<ranges>
+
+using std:: cout; using std::format; using std::accumulate; using std::string;
+using std::views; using std::array;
+
+int main(){
+  auto showValues{
+    [](auto& values, const std::string& message){
+      cout<<format("{}: ",message);
+
+      for(const auto& value : values){
+        cout<<format("{} ",value);
+      }
+      cout<<'\n';
+    }
+  };
+
+auto values1{std::views::iota(1,11)};
+showValues(values1, "Generate integers 1-10");
+
+auto values2{values1 | views::filter([](const auto& x){return x% 2 == 0;})};
+showValues(values2,"Filtering even integers");
+
+auto values3{ values2 | views::transform([](const auto& x){return x * x;}) };
+showValues(values,"Mapping even integers to squares");
+
+auto values4{ values1 | views::filter([](const auto& x){return x % 2 == 0;})
+                      | views::transform([](const auto& x){return x * x;})};
+showValues(values4, "Squares of even integers");
+
+}
