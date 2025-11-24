578714dd6889f5476faf23e1e91fe9f2118071ef
shavaun1 <jshavaun@yahoo.com>
2025-11-24 21:29:44 +0300

    update

--- chapters/Overloading/myarray.cpp
+++ chapters/Overloading/myarray.cpp
@@ -4,6 +4,7 @@
 #include<utility> //for std::move
 #include"point/MyArray.h"
 using std::format; using std::cout; using std::invalid_argument; using std::move; using std::cin;
+using std::out_of_range;
 
 //function to return a MyArray by value
 MyArray getArrayByValue()
@@ -38,4 +39,39 @@ int main()
   {
     cout<<"ints1 and ints2 are not equal\n\n";
   }
+
+  //create MyArray ints3 by copying ints1
+  MyArray ints3{ints1}; // invokes copy constructor
+
+  //print ints3 size and constents
+  cout<<format("\nints3 size: {}\ncontents: ", ints3.size())
+    <<int3; 
+
+  //use overloaded copy assignment (=) operator
+  cout<<"\n\nAssigning ints2 to ints1:\n";
+
+  if(ints1 == ints2)
+  {
+    cout<<"ints1 and ints2 are equal\n\n";
+  }
+
+  //use overloaded subscript operator to create an rvalue
+  cout<<format("ints1[5] is {}\n\n", ints1[5]);
+
+  //uses overloaded subscript operator to create an lvalue
+  cout<<"Assigning 1000 to ints1[5]\n";
+  ints1[5] = 1000;
+  cout<<"ints1: "<<ints1;
+
+  //attempt to use out-of-range subscript
+  try
+  {
+    cout<<"\n\nAttempt to assign 1000 to ints1[15]\n";
+    ints1[15] = 1000; //error: subscript out of range;
+  }
+  catch(const out_of_range& ex)
+  {
+    cout<<format("An exception occured: {}\n", ex.what());
+  }
+
 }
