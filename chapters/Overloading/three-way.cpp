6d8e86678a675b4d0d324f86aceecdfd519a7b06
shavaun1 <jshavaun@yahoo.com>
2025-12-03 15:07:19 +0300

    update

---
+++ chapters/Overloading/three-way.cpp
@@ -0,0 +1,68 @@
+#include<compare>
+#include<format>
+#include<iostream>
+#include<string>
+
+using std::cout; using std::format; using std::string; 
+
+class Time
+{
+  public:
+    Time(int hr, int min, int sec) noexcept
+      : m_hr{hr}, m_min{min}, m_sec{sec}{}
+
+    string toString() const 
+    {
+      return format("hr = {}, min={}, sec={}", m_hr, m_min, m_sec);
+    }
+
+    //<=> operator automatically supports equality/relational operators
+    auto operator<=>(const Time& T) const noexcept = default;
+
+  private:
+    int m_hr{0};
+    int m_min{0};
+    int m_sec{0};
+};
+
+int main()
+{
+  const Time t1(12, 15, 30);
+  const Time t2(12,15,30);
+  const Time t3(6,30,0);
+
+  cout<<format("t1: {}\nt2: {}\nt3: {}\n\n", t1.toString(), t2.toString(),
+      t3.toString());
+
+
+//using teh equality and relational operators
+cout<<format("t1 == t2:{}\n", t1 == t2);
+cout<<format("t1 != t2: {}\n", t1 != t2);
+cout<<format("t1 <  t2: {}\n",t1 < t2);
+cout<<format("t1 <= t2: {}\n", t1 <= t2);
+cout<<format("t1 > t2: {}\n", t1 > t2);
+cout<<format("t1 >= t2: {}\n", t1 >= t2);
+
+cout<<format("t1 == t3: {}\n", t1 == t3);
+cout<<format("t1 != t3: {}\n", t1 != t3);
+cout<<format("t1 < t3: {}\n", t1 < t3);
+cout<<format("t1 <= t2: {}\n", t1 <= t3);
+cout<<format("t1 > t3: {}\n", t1 > t3);
+cout << format("t1 >= t3: {}\n", t1 >= t3);
+
+//using <=> to perform comparisons
+if((t1 <=> t2) == 0)
+{
+  cout<<"t1 is equal to t2\n";
+}
+
+if((t1 <=> t3)>0)
+{
+  cout<<"t1 is greater than t3\n";
+}
+
+if((t1 <=> t3)<0)
+{
+  cout<<"ts is less than t1\n";
+}
+}
