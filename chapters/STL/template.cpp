46e36b60ad2707f2c3453ccf73782572a6957658
shavaun1 <jshavaun@yahoo.com>
2026-05-02 10:08:27 -0500

    update

--- chapters/STL/template.cpp
+++ chapters/STL/template.cpp
@@ -4,7 +4,7 @@
 #include<list>//list class-template definition
 #include<vector>
 
-using std::list; using std::ostream_iterator; using std::cout;
+using std::list; using std::ostream_iterator; using std::cout; using std::vector;
 
 //printlist function template definition; uses
 //ostream_iterator and copy algorithm to output list elements
@@ -39,4 +39,62 @@ int main()
   values.sort();
   cout<<"\nvalues after sorting contains: ";
   printList(values);
+   
+  //insert elements if ints into otherValues
+  vector ints{2,6,4,8};
+  list<int> otherValues{};//create list of ints
+  otherValues.insert(otherValues.cbegin(), ints.cbegin(), ints.cend());
+  cout<<"\nAfter insert, otherValues contains: ";
+  printList(otherValues);
+
+  //remove otherValues elements and insert at end of values
+  values.splice(values.cend(), otherValues);
+  cout<<"\nAfter splice, values contains: ";
+  printList(values);
+
+  values.sort(); //sort values
+  cout<<"nAfter sort, values conatins: ";
+  printList(values);
+
+  //insert elements of ints into otherValues
+  otherValues.insert(otherValues.cbegin(), ints.cbegin(),ints.cend());
+  otherValues.sort(); //sprt the list
+  cout<<"\nAfter insert and sort, otherValues contains: ";
+  printList(otherValues);
+
+  //remove otherValues elements and insert into values in sorted order
+  values.merge(otherValues);
+  cout<<"\nAfter merge:\n values contains: ";
+  printList(values);
+  cout<<"\n otherValues contains: ";
+  printList(otherValues);
+
+  values.pop_front(); //remove elements from front
+  values.pop_back(); //remove elements from back
+  cout<<"\nAfter pop_front and pop_back: \n values contains: ";
+  printList(values);
+  values.unique(); //remove duplicate elements
+  cout<<"\nAfter unique, values contains: ";
+  printList(values);
+
+  values.swap(otherValues); //swap elements of values and otherValues
+  cout<<"\nAfter swap:\n values contains: ";
+  printList(values);
+  cout<<"\n otherValues contains: ";
+  printList(otherValues);
+
+  //replace contents of values with elements of otherValues
+  values.assign(otherValues.cbegin(), otherValues.cend());
+  cout<<"\nAfter assign, values contains: ";
+  printList(values);
+
+  //remove otherValues elements and insert into values in sorted order
+  values.merge(otherValues);
+  cout<<"\nAfter merge, values contains: ";
+  printList(values);
+
+  values.remove(4); //remove all 4s
+  cout<<"\nAfter remove(4), values contains: ";
+  printList(values);
+  cout<<"\n";
 }
